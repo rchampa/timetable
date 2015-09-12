53b4f52a25c6a25371eb892bef8973effdf3efdb
Ricardo Champa <ricardochampa@gmail.com>
2015-09-13 01:38:56 +0200

    fixing wsgi

--- wsgi.py
+++ wsgi.py
@@ -2,17 +2,17 @@
 import os
 
 
-if os.environ.get('OPENSHIFT_MYSQL_DB_URL') is not None:
-  #virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-  virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
-  virtualenv = os.path.join(virtenv, 'venv/bin/activate_this.py')
+# if os.environ.get('OPENSHIFT_MYSQL_DB_URL') is not None:
+#   #virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+#   virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
+#   virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 
-  with open(virtualenv, 'rb') as exec_file:
-    file_contents = exec_file.read()
+#   with open(virtualenv, 'rb') as exec_file:
+#     file_contents = exec_file.read()
 
-  compiled_code = compile(file_contents, virtualenv, 'exec')
-  exec_namespace = dict(__file__=virtualenv)
-  exec(compiled_code, exec_namespace)
+#   compiled_code = compile(file_contents, virtualenv, 'exec')
+#   exec_namespace = dict(__file__=virtualenv)
+#   exec(compiled_code, exec_namespace)
   #
   # IMPORTANT: Put any additional includes below this line.  If placed above this
   # line, it's possible required libraries won't be in your searchable path
