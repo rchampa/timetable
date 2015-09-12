dac011192d9bd73f8f1b5475dfd31c518bc864ad
Ricardo Champa <ricardochampa@gmail.com>
2015-09-12 10:33:14 +0200

    push to openshift

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 import os
 
-#virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-# virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
-# virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-# try:
-#     execfile(virtualenv, dict(__file__=virtualenv))
-# except IOError:
-#     pass
-#
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
+
+if os.environ.get('OPENSHIFT_MYSQL_DB_URL') is not None:
+  #virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+  virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
+  virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+
+  with open(virtualenv, 'rb') as exec_file:
+    file_contents = exec_file.read()
+
+  compiled_code = compile(file_contents, virtualenv, 'exec')
+  exec_namespace = dict(__file__=virtualenv)
+  exec(compiled_code, exec_namespace)
+  #
+  # IMPORTANT: Put any additional includes below this line.  If placed above this
+  # line, it's possible required libraries won't be in your searchable path
+  #
 
 from flaskapp import app as application
 
