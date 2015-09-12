05c56b8a4a8b33a97692aa3adf2cc821cdcf33dc
Ricardo Champa <ricardochampa@gmail.com>
2015-09-12 11:44:33 +0200

    push to openshift

--- db.sql
+++ db.sql
@@ -20,13 +20,17 @@ create table usuarios
     ultima_conexion datetime
 );
 
+#http://localhost:5000/schedules/2/02-09-2015
+#http://localhost:5000/schedules/id_usuario/fecha
 create table tablas
 (
 	id_tabla int(8) AUTO_INCREMENT primary key,
 	id_usuario int(8),
 	descripcion varchar(120),
 	semana_del_anio int, # la semana del año siempre es unica, se tendrá en cuenta como clave, al menos internamente.
+	anio int,
 	borrado boolean,
+	CONSTRAINT uc_semana_anio UNIQUE (semana_del_anio,anio),
 	FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
 
@@ -37,15 +41,20 @@ create table eventos
 	fecha datetime,
 	borrado boolean,
 
-	status int(2),
-	color varchar(7),
-	comienza varchar(5),
-	finaliza varchar(5),
+	status int(2),#0 no validado, 1 validado
+	privado int(2),#0 privado, 1 publico
+	color varchar(13), #RGBA
+	comienza varchar(5),#19:00
+	finaliza varchar(5),#20:00
 	titulo varchar(30),
 	descripcion varchar(100),
 	url_imagen varchar(100), # intentaré que sean cortas
 	dia int(1), # es mejor un numero del 1 al 7
-	lugar varchar(50),# aqui molaria poner algo de google maps, no?
+	direccion varchar(100),
+	latitud float,
+	longitud float,
+	lugar varchar(50),# descripcion del lugar
+	#datos precalculados para la carga del evento
 	timediff_h varchar(2),
 	timediff_inmins varchar(4),
 	timediff_m varchar(2),
