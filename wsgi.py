e1a3ba91a6a7fd358234921122a9ace474fed330
Ricardo Champa <ricardochampa@gmail.com>
2015-09-13 01:18:54 +0200

    fixing wsgi

--- wsgi.py
+++ wsgi.py
@@ -26,8 +26,7 @@ from flaskapp import app as application
 #
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    #httpd = make_server('0.0.0.0', 5000, application)
-    httpd = make_server('localhost', 8051, application)
+    httpd = make_server('0.0.0.0', 5000, application)
     # Wait for a single request, serve it and quit.
     #httpd.handle_request()
     httpd.serve_forever()
