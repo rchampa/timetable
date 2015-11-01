565bd5efcf12ccce6b81f83d6d3e05adfe051574
Ricardo Champa <ricardochampa@gmail.com>
2015-11-01 14:53:27 +0100

    checkpoint

--- static/js/home.js
+++ static/js/home.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
 
     var token = localStorage.getItem('token');
     $.ajax({
-        type: "POST",
+        type: "GET",
         url: "http://timetable-ttv1.rhcloud.com/users/2",//url login -> rhcloud.com
-        data: JSON.stringify(usuario),
         contentType: "application/json; charset=utf-8",
         beforeSend: function (xhr) {
           xhr.setRequestHeader("Authorization", "Bearer "+token)
